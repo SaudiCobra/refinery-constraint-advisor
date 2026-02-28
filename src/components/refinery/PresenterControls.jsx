009c1be11947fc30ffce30287e3163ae85ad1346
Python “Aikido Confidential” Saad <sscorpionsaad@gmail.com>
2026-02-28 01:59:29 +0000

    File changes

--- src/components/refinery/PresenterControls.jsx
+++ src/components/refinery/PresenterControls.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef, useCallback } from "react";
 import { SCENARIOS } from "./calcEngine";
 
 // Brief descriptors per scenario index
