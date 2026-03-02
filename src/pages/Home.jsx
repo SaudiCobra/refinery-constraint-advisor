d2c96e9828a99daa0cc7a84153fe2dddef5e84ce
worlitwontmatter <worlitwontmatter@gmail.com>
2026-03-02 17:23:56 +0000

    File changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -24,6 +24,7 @@ import {
   computeAllConstraints,
   getNearestConstraint,
   getSystemState,
+  computeMultiVarTTL as computeMultiVarTTLEngine,
   getEscalationLevel,
   getAlarmState,
   getRecommendation,
